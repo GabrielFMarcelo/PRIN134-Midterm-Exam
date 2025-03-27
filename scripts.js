9cbc5b9af2367d3b0d7902d88fd5dd43f8ca50d2
Gabriel Marcelo <marcelo2235888@mls.ceu.edu.ph>
2025-03-27 15:23:29 +0800

    Finished Midterm Exam

--- scripts.js
+++ scripts.js
@@ -1,17 +1,5 @@
-const petButtons = document.querySelectorAll(".btn-full");
-const selectAllBtn = document.querySelector("#btn-select-all");
-const unselectAllBtn = document.querySelector("#btn-unselect-all");
-const firstBtn = document.querySelector("#btn-select-first");
-const lastBtn = document.querySelector("#btn-select-last");
-const prevBtn = document.querySelector("#btn-select-previous");
-const nextBtn = document.querySelector("#btn-select-next");
-
-const banner = document.querySelector(".banner-content");
-const cards = document.querySelectorAll(".card");
-
-let selectedIndex = 0; 
-
 // 1. When PET button is clicked, icon changes accordingly to corresponding state.
+const petButtons = document.querySelectorAll(".btn-full");
 petButtons.forEach((btn) => {
     btn.addEventListener("click", () => {
         const icon = btn.querySelector("i");
@@ -21,6 +9,7 @@ petButtons.forEach((btn) => {
 });
 
 // 2. When PET ALL button is clicked, all PET button icon change into a solid icon.
+const selectAllBtn = document.querySelector("#btn-select-all");
 selectAllBtn.addEventListener("click", () => {
     petButtons.forEach((btn) => {
         const icon = btn.querySelector("i");
@@ -30,6 +19,7 @@ selectAllBtn.addEventListener("click", () => {
 })
 
 // 3. When UNPET ALL button is clicked, all PET button icon change into a regular icon.
+const unselectAllBtn = document.querySelector("#btn-unselect-all");
 unselectAllBtn.addEventListener("click", () => {
     petButtons.forEach((btn) => {
         const icon = btn.querySelector("i");
@@ -38,6 +28,9 @@ unselectAllBtn.addEventListener("click", () => {
     })
 })
 
+const cards = document.querySelectorAll(".card");
+let selectedIndex = 0; 
+
 function updateSelection(index) {
     document.querySelector(".card-selected")?.classList.remove("card-selected", "active");
     cards[index].classList.add("card-selected", "active");
@@ -45,18 +38,21 @@ function updateSelection(index) {
 }
 
 // 4. When Last button >> is clicked, the last card from the gallery will be highlighted.
+const lastBtn = document.querySelector("#btn-select-last");
 lastBtn.addEventListener("click", () => {
     updateSelection(cards.length - 1);
 });
 
 
 // 5. When First button << is clicked, the first card from the gallery will be highlighted.
+const firstBtn = document.querySelector("#btn-select-first");
 firstBtn.addEventListener("click", () => {
     updateSelection(0);
 });
 
 // 6. When Next button > is clicked, the highlighted card will be the next item from the currently selected.
 //     * If the last item is currently selected, then next button is clicked, the highlight card will be moved to the first card from the gallery.
+const nextBtn = document.querySelector("#btn-select-next");
 nextBtn.addEventListener("click", () => {
     let nextIndex = (selectedIndex + 1) % cards.length;
     updateSelection(nextIndex);
@@ -65,6 +61,7 @@ nextBtn.addEventListener("click", () => {
 
 // 7. When Previous button < is clicked, the highlighted card will be the previous item from the currently selected.
 //     * If the last item is currently selected, then next button is clicked, the highlight card will be moved to the first card from the gallery.
+const prevBtn = document.querySelector("#btn-select-previous");
 prevBtn.addEventListener("click", () => {
     let prevIndex = (selectedIndex - 1 + cards.length) % cards.length;
     updateSelection(prevIndex);
@@ -73,6 +70,7 @@ prevBtn.addEventListener("click", () => {
 
 // 8. When Site Logo "BARK!-ada" from the banner is clicked, an animation will display on the site logo.
 //     * Can reference to https://animate.style/
+const banner = document.querySelector(".banner-content");
 banner.addEventListener("click", () => {
     banner.classList.add("animate__animated", "animate__bounce");
     setTimeout(()=> {
